c135d3ef2bda9e5c14037a7f5364eb930a3cb43d
Ved Nig <vednig12@gmail.com>
2024-03-12 21:34:07 +0530

    Fix Example for Linux

--- examples/helowrld.py
+++ examples/helowrld.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import shadowTk
+import platform
 
 root = tk.Tk()
 
@@ -10,9 +11,11 @@ shadowTk.Shadow(message,color='#ccffcc', size=1.3, offset_x=-5,onhover={'color':
 message2 = tk.Label(root, text="Hello, World!",bg='#ccffcc',fg='#000000',font=('Arial', 30))
 shadowTk.Shadow(message2,color='#1565C0', size=1.3, offset_x=-5,onhover={'color':'#3265C0'})
 message2.pack()
-
-from ctypes import windll
-try:
-    windll.shcore.SetProcessDpiAwareness(1)
-finally:
+if(platform.system()=='Windows'):    
+    from ctypes import windll
+    try:
+        windll.shcore.SetProcessDpiAwareness(1)
+    finally:
+        root.mainloop()
+else:
     root.mainloop()
